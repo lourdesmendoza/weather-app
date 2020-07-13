74a779ff1285ac942335fc4738778e2d76e39181
Lourdes Mendoza <lmendoza@teco.com.ar>
2020-07-12 22:27:14 -0300

    Created a form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = () => {
+
+    //Form state
+    const [search, setSearch] = useState({
+        city: '',
+        country:''
+    });
+
+    //Get city and country
+    const {city, country} = search;
+
+    //Function that places the elements in the state
+    const handleChange = e => {
+        //Updated state
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.name
+        })
+    }
+
     return ( 
         <form>
             <div className="input-field col s12">
@@ -8,6 +27,8 @@ const Form = () => {
                     type="text"
                     name="city"
                     id="city"
+                    value={city}
+                    onChange={handleChange}
                 /> 
                 <label htmlFor="city">City: </label>
             </div>
@@ -16,10 +37,19 @@ const Form = () => {
                 <select
                     name="country"
                     id="country"
+                    value={country}
+                    onChange={handleChange}
                 >
                     <option value="">
                         -- Select a country --
                     </option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="country">Country: </label>
             </div>
