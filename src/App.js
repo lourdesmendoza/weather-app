d45dd3d1044000feb3d8831a0b1b06641cd68bdf
Lourdes Mendoza <lmendoza@teco.com.ar>
2020-07-12 22:18:03 -0300

    Created a form

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {Fragment} from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+
+function App() {
+	return (
+		<Fragment>
+			<Header 
+				title='Weather app'
+			/>
+			
+			<div className="content-form">
+				<div className="container">
+					<div className="row">
+						<div className="col m6  s12">
+							<Form />
+						</div>
+
+						<div className="col m6  s12">
+							2
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+		</Fragment>
+	);
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = ({title}) => {
+    return ( 
+        <nav>
+            <div className="nav-wrapper teal darken-2">
+                <a href="#!" className="brand-logo">
+                    {title}
+                </a>
+
+            </div>
+        </nav>
+     );
+}
+ 
+export default Header;
